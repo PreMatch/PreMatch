c1f786ca85c4611f38e7cad57ef3997ddccf54bd
Michael Peng <michael@mcmoo.org>
2018-08-19 10:55:56 -0400

    Formalize errors and add requirements.txt

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import atexit
 import database
 import json
 from teachers import teachers
-from google_auth import *
+from google_auth import validate_token_for_info
 
 PERIODS = list(map(chr, range(65, 73)))
 
@@ -14,6 +14,14 @@ def empty(string):
 
 def missing_form_field(key):
     return key not in flask.request.form or empty(flask.request.form[key])
+
+def error(code, message):
+    return flask.render_template('error.html', code=code, error=message), code
+
+def json_success(message):
+    return flask.jsonify({'status': 'success', 'message': str(message)})
+def json_failure(code, message):
+    return flask.jsonify({'status': 'failure', 'message': str(message)}), code
 
 app = flask.Flask(__name__)
 
@@ -24,7 +32,7 @@ def frontpage():
 @app.route('/user/<handle>', methods=['GET'])
 def show_user(handle):
     if not database.handle_exists(handle):
-        return '404 Page not Found: no such handle', 404
+        return error(404, '404 No such handle')
     schedule = database.user_schedule(handle)
     name = database.user_name(handle)
     return flask.render_template('user.html', schedule=schedule, name=name, handle=handle, teachers=teachers)
@@ -37,15 +45,15 @@ def do_add():
 def do_update():
     # Form key existence check
     if missing_form_field('id_token'):
-        return 'Missing value for id_token', 400
+        return json_failure(400, 'Missing value for id_token')
     if any(map(missing_form_field, PERIODS)):
-        return 'One or more periods missing', 400
+        return json_failure(400, 'One or more periods missing')
     
     # Teacher validity check
     new_schedule = list(map(flask.request.form.get, PERIODS))
     for teacher in new_schedule:
         if teacher not in teachers:
-            return 'Unknown teacher: ' + teacher, 400
+            return error(400, 'Unknown teacher: ' + teacher)
     try:
         # Token validity check
         token = flask.request.form['id_token']
@@ -53,22 +61,24 @@ def do_update():
 
         if database.handle_exists(handle):
             database.update_schedule(handle, new_schedule)
+            return json_success('Schedule updated successfully')
         else:
             database.add_schedule(handle, name, new_schedule)
-        return '{"status": "success"}'
+            return json_success('Schedule added successfully')
+
     except Exception as e:
-        return json.dumps({'status': 'error', 'message': str(e)})
+        return error(500, str(e))
 
 @app.route('/roster/<period>/<teacher>')
 def show_roster(period, teacher):
     if period not in PERIODS:
-        return 'Invalid period: ' + period, 400
+        return error(400, 'Invalid period: ' + period)
     if teacher not in teachers:
-        return 'Invalid teacher: ' + teacher, 400
+        return error(400, 'Invalid teacher: ' + teacher)
 
     roster = database.class_roster(period, teacher)
     if len(roster) == 0:
-        return 'Empty or nonexistent class'
+        return error(400, 'Empty or nonexistent class')
 
     return flask.render_template('roster.html', period=period, teacher=teacher, roster=roster)
 
