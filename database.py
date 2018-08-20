1bc97d6fbb8bc430de331574cefe21c8bcd8001a
PlantsVsDan <Soccer.Ivanovich@gmail.com>
2018-08-19 22:54:51 -0400

    Apply PEP, add hero to about, general formatting

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask import g
 
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -8,40 +9,50 @@ def get_db():
         db.row_factory = sqlite3.Row
     return db
 
+
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+
 def modify_db(query, args=()):
     get_db().execute(query, args)
     get_db().commit()
 
+
 def handle_exists(handle):
     return query_db('select A from schedule where handle is ?', [handle], one=True) is not None
+
 
 def remove_schedule(handle):
     modify_db('delete from schedule where handle is ?', [handle])
 
+
 def add_schedule(handle, name, sched_list):
-    if (handle_exists(handle)):
+    if handle_exists(handle):
         raise Exception('Schedule with handle {} already exists'.format(handle))
     modify_db('insert into schedule values (?,?,?,?,?,?,?,?,?,?)', [handle, name] + sched_list)
 
+
 def update_schedule(handle, sched_list):
     modify_db('update schedule set A=?,B=?,C=?,D=?,E=?,F=?,G=?,H=? where handle is ?',
-        list(sched_list) + [handle])
+              list(sched_list) + [handle])
+
 
 def user_schedule(handle):
     return query_db('select A,B,C,D,E,F,G,H from schedule where handle is ?', [handle], one=True)
 
+
 def user_name(handle):
     return query_db('select name from schedule where handle is ?', [handle], one=True)['name']
+
 
 def class_roster(period, teacher):
     result = query_db('select name,handle from schedule where {} is ?'.format(period), (teacher,))
     return result
+
 
 def search_user(query):
     pattern = '%' + query + '%'
--- google_auth.py
+++ google_auth.py
@@ -4,13 +4,14 @@ from google.auth.transport import requests
 CLIENT_ID = '764760025104-is3262o216isl5tbfj4aakcel2tirl7n.apps.googleusercontent.com'
 APS_DOMAIN = 'k12.andoverma.us'
 
+
 # returns (handle, name)
 def validate_token_for_info(token):
-	idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+    idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
 
-	if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-		raise ValueError('Wrong issuer')
-	if 'hd' not in idinfo or idinfo['hd'] != APS_DOMAIN:
-		raise ValueError('You need to use your k12.andoverma.us account')
+    if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+        raise ValueError('Wrong issuer')
+    if 'hd' not in idinfo or idinfo['hd'] != APS_DOMAIN:
+        raise ValueError('You need to use your k12.andoverma.us account')
 
-	return idinfo['email'].split('@')[0], idinfo['name']
+    return idinfo['email'].split('@')[0], idinfo['name']
