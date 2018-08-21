5752525bbfe3b6cfa9db5aed2f6210246355e952
PlantsVsDan <Soccer.Ivanovich@gmail.com>
2018-08-20 23:11:25 -0400

    Schedule adding

--- static/js/addSchedule.js
+++ static/js/addSchedule.js
@@ -1,53 +1,46 @@
-function buildForm(periods, teachers) {
-    let form = document.getElementById('form');
-
-    periods.forEach((period) => {
-        let div = document.createElement('div');
-        div.setAttribute("class", "teacher-dropdown");
-
-        let contentDiv = document.createElement('div');
-        contentDiv.setAttribute("class", "teacher-dropdown-content");
-        contentDiv.setAttribute('id', `teacher-dropdown${period}`)
-
-        let textBox = document.createElement('input');
-        textBox.setAttribute("type", "text");
-        textBox.setAttribute("placeholder", "Search for a teacher...");
-        textBox.setAttribute("id", `teacherInput${period}`);
-        textBox.setAttribute("onkeyup", `filterTeachers("${period})"`);
-
-        let labelElement = document.createElement('label');
-        labelElement.setAttribute("for", `teacherInput${period}`);
-        labelElement.innerHTML = `Block ${period}: `;
-
-        form.appendChild(div);
-        div.appendChild(contentDiv);
-        contentDiv.appendChild(labelElement);
-        contentDiv.appendChild(textBox);
-
-        for (let i = 0; i < teachers.length; i++) {
-            let teacher = teachers[i];
-            let a = document.createElement('a');
-            a.innerHTML = teacher;
-            contentDiv.appendChild(a);
-        }
-    });
-
-    let submit = document.createElement('input');
-    submit.setAttribute("type", "submit");
-    form.appendChild(submit);
-}
+let selectedTeachers = [];
 
 function filterTeachers(period) {
     let input, filter, a, i;
     input = document.getElementById(`teacherInput${period}`);
     filter = input.value.toUpperCase();
+
+    let linkHolder = document.getElementById(`link-holder-${period}`);
+
+    if (filter.length > 0) {
+        linkHolder.style.display = "Block";
+    } else {
+        linkHolder.style.display = "None";
+    }
     let div = document.getElementById(`teacher-dropdown${period}`);
     a = div.getElementsByTagName("a");
     for (i = 0; i < a.length; i++) {
-        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1 && filter !== '') {
+            a[i].style.display = "block";
         } else {
             a[i].style.display = "none";
         }
     }
+}
+
+function selectInput(period) {
+    $("#form").children().each((index) => {
+        let contentDiv = document.getElementById('form').children[index].children[0];
+        if (!contentDiv.id.endsWith(period)) {
+            contentDiv.getElementsByTagName('div')[0].style.display = "None";
+        }
+    });
+}
+
+function selectTeacher(period, teacher) {
+    // TODO: Some sort of visual effect, probably some sort of message
+
+    let form = document.getElementById('form-invis');
+
+    let input = form.querySelector(`#input-invis-${period}`);
+
+    if (input.id.endsWith(period)) {
+        input.setAttribute('value', teacher);
+    }
+
 }
