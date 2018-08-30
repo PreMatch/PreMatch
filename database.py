b283f7a94e9a7641ec92ea9b00dd3060486df672
Michael Peng <michael@mcmoo.org>
2018-08-29 20:55:09 -0400

    Implement common classmate algorithm

--- database.py
+++ database.py
@@ -70,11 +70,15 @@ def user_name(handle):
     return None if row is None else row['name']
 
 
-def class_roster(period, teacher):
+def users_in_class(period, teacher):
     query = db_query()
     query.add_filter(period, '=', teacher)
 
-    return list(map(lambda row: (row['name'], row['handle']), list(query.fetch())))
+    return list(query.fetch())
+
+
+def class_roster(period, teacher):
+    return list(map(lambda row: (row['name'], row['handle']), users_in_class(period, teacher)))
 
 
 def search_user(search_query):
@@ -83,3 +87,33 @@ def search_user(search_query):
 
     return list(filter(lambda user:
                        search_query.lower() in user['name'].lower() or search_query.lower() in user['handle'], users))
+
+
+def common_classmates(handle):
+    frequency = {}
+    max_hit_rate = 0
+    max_users = []
+
+    schedule = user_schedule(handle)
+
+    for period in PERIODS:
+        for classmate in users_in_class(period, schedule[period]):
+            u_handle = classmate['handle']
+            if u_handle != handle:
+
+                if u_handle not in frequency:
+                    frequency[u_handle] = 1
+                else:
+                    frequency[u_handle] += 1
+
+                new_freq = frequency[u_handle]
+                if new_freq > max_hit_rate:
+                    max_hit_rate = new_freq
+                    max_users = [classmate]
+                elif new_freq == max_hit_rate:
+                    max_users.append(classmate)
+
+    if max_hit_rate < 2:
+        return None
+
+    return list(map(lambda i: i['name'], max_users)), max_hit_rate
