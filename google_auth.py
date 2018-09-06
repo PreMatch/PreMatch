78f9c6b9fc7e9e7cdfef5d11ba0a30155a5e83fa
Michael Peng <michael@mcmoo.org>
2018-09-05 23:47:57 -0400

    Add graduation year checks

--- google_auth.py
+++ google_auth.py
@@ -1,8 +1,16 @@
 from google.oauth2 import id_token
 from google.auth.transport import requests
+import datetime
 
 CLIENT_ID = '764760025104-is3262o216isl5tbfj4aakcel2tirl7n.apps.googleusercontent.com'
 APS_DOMAIN = 'k12.andoverma.us'
+
+
+def grad_year_of_handle(handle):
+    year_str = ''.join(filter(lambda char: char.isdigit(), handle))[:4]
+    if len(year_str) == 0:
+        return None
+    return int(year_str)
 
 
 # returns (handle, name)
@@ -14,4 +22,12 @@ def validate_token_for_info(token):
     if 'hd' not in idinfo or idinfo['hd'] != APS_DOMAIN:
         raise ValueError('You need to use your k12.andoverma.us account')
 
-    return idinfo['email'].split('@')[0], idinfo['name']
+    handle = idinfo['email'].split('@')[0]
+    grad_year = grad_year_of_handle(handle)
+    this_year = datetime.date.today().year
+
+    if grad_year is not None and (grad_year < this_year or
+                                  grad_year - 4 > this_year):
+        raise ValueError('You are not a current student of Andover High School')
+
+    return handle, idinfo['name']
