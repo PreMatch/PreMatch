5f867bd956c78e4b8cadcd7a30133688c7d3c478
Daniel Ivanovich <dan@ivanovi.ch>
2019-08-18 15:10:14 -0400

    Setup scrollable navigation links, restyle /about to solid background

--- static/js/macroScripts.js
+++ static/js/macroScripts.js
@@ -1,18 +1,22 @@
 function setActive(href) {
-    let sidenav = document.getElementById('sidenav');
+    let sidenav = document.getElementById('sidenav-link-container');
     let links = sidenav.children;
     let activeIndex = -1;
     for (let i = 0; i < links.length; i++) {
         let currentLink = links[i];
         if (currentLink.getAttribute("href") === href) {
             currentLink.classList.add("sidenav-active");
+
+            if($('#body').hasClass('bg-primary'))
+                currentLink.classList.add('inverted');
+
             activeIndex = i;
             break;
         }
     }
 
     for (let i = 0; i < links.length; i++) {
-        if (i !== activeIndex)
+        if (i !== activeIndex && !$('#body').hasClass('bg-primary'))
             links[i].classList.add("overridden-white");
     }
 
